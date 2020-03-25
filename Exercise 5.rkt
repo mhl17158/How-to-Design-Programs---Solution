b21002d26611fb09f60aaf1611ea578cd21015c4
mhl17158 <hashim.liaquat@yahoo.com>
2020-03-25 16:57:07 +0500

    Commit 2

---
+++ Exercise 5.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Exercise 5|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(define size 100)
+
+(overlay/xy(triangle(/ size 2)"solid""green")(/ size 5)(/ size 10)(rectangle(/ size 10)(/ size 2)"solid""brown"))
+
+
